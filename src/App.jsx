1f1d38e632f0d2e00933f4c56b7b4e5c8857a7e9
Kira <Kira@blanks-MacBook-Pro.local>
2024-02-28 15:24:04 -0500

    new colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Route, Routes, Link } from "react-router-dom";
 import Red from "./components/Red";
 import Blue from "./components/Blue";
+import Yellow from "./components/Yellow";
+import Purple from "./components/Purple";
+import Green from "./components/Green";
 import Home from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div id="container">
         <h1>Hello React Router!</h1>
         <div id="navbar">
           <Link to="/">Home</Link>
-
           <Link to="/blue">Blue</Link>
           <Link to="/red">Red</Link>
+          <Link to="/yellow">Yellow</Link>
+          <Link to="/purple">Purple</Link>
+          <Link to="/green">Green</Link>
         </div>
+
         <div id="main-section">
           <Routes>
             <Route path="/" element={<Home></Home>}></Route>
             <Route path="/blue" element={<Blue></Blue>}></Route>
             <Route path="/red" element={<Red></Red>}></Route>
+            <Route path="/yellow" element={<Yellow></Yellow>}></Route>
+            <Route path="/purple" element={<Purple></Purple>}></Route>
+            <Route path="/green" element={<Green></Green>}></Route>
           </Routes>
         </div>
       </div>
